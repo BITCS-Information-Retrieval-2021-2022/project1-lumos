33488683eafa4c1439814bcef4ec808824fb8122
Amelia47 <897134939@qq.com>
2021-12-25 23:29:10 +0800

    search module

--- search/insert.py
+++ search/insert.py
@@ -13,19 +13,18 @@ f = open("config_retrieval")
 for line in f.readlines():
 
     component = line.strip().split(" = ")
-    if component[0]== "host":
-        host=component[1]
+    if component[0] == "host":
+        host = component[1]
     elif component[0] == "db":
-        db=component[1]
+        db = component[1]
     elif component[0] == "port":
-        port=component[1]
+        port = component[1]
     elif component[0] == "es_path":
         es_path = component[1]
     elif component[0] == "es_port":
         es_port = component[1]
 
-
-myclient = pymongo.MongoClient('mongodb://' + host + ':'+port+'/')
+myclient = pymongo.MongoClient('mongodb://' + host + ':' + port + '/')
 
 mydb = myclient[db]
 
@@ -50,11 +49,9 @@ def toelastic(db, es):
     cnt = 0
     for x in db.find({}, {"_id": 0, "video_download": 0, "pdf_download": 0, "pdfVisit": 0, "videoVisit": 0}):
         cnt += 1
-        #if cnt > 10:
-            #break
         st = x['year']
-        author=x['authors']
-        if(not isinstance(author,list)):
+        author = x['authors']
+        if (not isinstance(author, list)):
             authors = [x.strip() for x in author.split(",")]
             x['authors'] = authors
         if (st != '' and is_number(st) and int(st) < 10):
@@ -65,22 +62,20 @@ def toelastic(db, es):
 
 if __name__ == '__main__':
 
-    es = Elasticsearch(es_path + ':' +es_port)
+    es = Elasticsearch(es_path + ':' + es_port)
 
     template = open('index_template.json', encoding='utf-8')
 
     mappings = json.load(template)
-    print(mappings)
 
     if es.indices.exists(index='paper') is not True:
         res = es.indices.create(index='paper', body=mappings)
     else:
         es.indices.delete(index='paper')
         res = es.indices.create(index='paper', body=mappings)
-
     mydb.list_collections()
     collist = mydb.list_collection_names()
     for sites in collist:  # 判断 sites 集合是否存在
         print("集合" + sites + "已存在！")
         if "BasicInfo" in sites:
-            toelastic(mydb[sites],es)
+            toelastic(mydb[sites], es)
